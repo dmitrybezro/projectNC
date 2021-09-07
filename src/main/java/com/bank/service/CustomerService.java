a60862635cd892c42c7507af721c631516788538
dmitrybezro <d.bezrodnow@gmail.com>
2021-09-07 18:14:23 +0300

    code restyled

--- src/main/java/com/bank/service/CustomerService.java
+++ src/main/java/com/bank/service/CustomerService.java
@@ -1,28 +1,14 @@
 package com.bank.service;
 
-import com.bank.converters.EntityDtoConverter;
-import com.bank.dto.ObjectDto;
 import com.bank.entity.Customer;
-import com.bank.dto.ValueDto;
 import com.bank.exception.CustomerNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.bank.repository.ObjectsRepository;
-import com.bank.repository.ValuesRepository;
-
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class CustomerService {
-    @Autowired
-    private ObjectsRepository objectsRepository;
-
-    @Autowired
-    private ValuesRepository valueRepository;
-
     @Autowired
     private EntityService entityService;
 
